4a9c33ebdce8c14a6524894011bed4ee523aef20
mrmarkolinus <72712091+mrmarkolinus@users.noreply.github.com>
2023-10-13 23:13:57 +0200

    cache reworked

--- src/BlogArticle.py
+++ src/BlogArticle.py
@@ -7,60 +7,71 @@ import logging
 
 class BlogArticle():
     
-    def __init__(self, llm, article_topic, workers, log_obj, cached=True):
+    def __init__(self, llm, article_topic, workers, log_obj, load_from_cache_if_possible=True):
         self._llm = llm
         self._workers = workers
         self._article_topic = article_topic
-        self._cached = cached
+        self._logger = log_obj
 
         self._editor = self._workers["editor"](self._llm, self._article_topic)
         self._writer = self._workers["writer"](self._llm)
 
-        if cached:
-            self._cache = BlogCache()
-
-        cache_exists, cached_status, cached_content = self._cache.load_cached_content()
-
-        skip_title_generation = False
-        skip_chapter_generation = False
-        skip_chapter_content_generation = False
         
-        if cache_exists:
-            if cached_status == "title_generated":
-                skip_title_generation = True
-            elif cached_status == "chapters_title_generated":
-                skip_title_generation = True
-                skip_chapter_generation = True
-            elif cached_status == "chapters_content_generated":
-                skip_title_generation = True
-                skip_chapter_generation = True
-                skip_chapter_content_generation = True
+        self._cache = BlogCache(load_from_cache_if_possible)
+
+        # cache_exists, cached_status, cached_content = self._cache.load_cached_content()
+
+        # skip_title_generation = False
+        # skip_chapter_generation = False
+        # skip_chapter_content_generation = False
+        
+        # if cache_exists:
+        #     if cached_status == "title_generated":
+        #         skip_title_generation = True
+        #     elif cached_status == "chapters_title_generated":
+        #         skip_title_generation = True
+        #         skip_chapter_generation = True
+        #     elif cached_status == "chapters_content_generated":
+        #         skip_title_generation = True
+        #         skip_chapter_generation = True
+        #         skip_chapter_content_generation = True
             
 
-        self._article_title, self._article_seo_keywords = self._editor.generate_article_title_and_keywords(self._article_topic)
-        log_obj.info("Article title: " + self._article_title)
-        log_obj.info("Artcicle keywords: " + self._article_seo_keywords)
+        self._generate_title_and_seo()
+        self._generate_chapters_title_and_description()
+        
 
-        self._article_chapters, self._article_chapters_header = self._editor.generate_article_chapters(self._article_title, self._article_seo_keywords)
-        log_obj.info("Article chapters: " + ', '.join(self._article_chapters))
-        log_obj.info("Article chapters headers: " + ', '.join(self._article_chapters_header))
-
-        if skip_chapter_content_generation:
-            self._chapter_content = cached_content
-        else:
-            self._chapter_content = []
-            last_chapter_content = " "
-            for (index, _) in enumerate(self._article_chapters):
-                self._chapter_content.append(self._writer.generate_article_chapter(self._article_title, self._article_seo_keywords, 
-                                                                                self._article_chapters, self._article_chapters_header, index,
-                                                                                last_chapter_content))
-                last_chapter_content = self._chapter_content[index]
-                log_obj.info("Chapter \"" + self._article_chapters[index] + "\" generated successfully")
-                log_obj.info(self._chapter_content[index])
+        self._chapter_content = []
+        last_chapter_content = " "
+        for (index, _) in enumerate(self._article_chapters):
+            self._chapter_content.append(self._writer.generate_article_chapter(self._article_title, self._article_seo_keywords, 
+                                                                            self._article_chapters, self._article_chapters_header, index,
+                                                                            last_chapter_content))
+            last_chapter_content = self._chapter_content[index]
+            log_obj.info("Chapter \"" + self._article_chapters[index] + "\" generated successfully")
+            log_obj.info(self._chapter_content[index])
 
 
         self._article_text_consolidated = self._editor.consolidate_article(self._article_title, self._article_seo_keywords, self._article_chapters, self._article_chapters_header, self._chapter_content)
 
+
+    def _generate_title_and_seo(self):
+        self._article_title, self._article_seo_keywords = self._editor.generate_article_title_and_keywords(self._article_topic)
+        self._logger.info("Article title: " + self._article_title)
+        self._logger.info("Artcicle keywords: " + self._article_seo_keywords)
+        self._cache.write_cache_article(self._cache.tag_title(), self._article_title)
+        self._cache.write_cache_article(self._cache.tag_seo_keywords(), self._article_seo_keywords)
+
+    def _generate_chapters_title_and_description(self):
+        self._article_chapters, self._article_chapters_header = self._editor.generate_article_chapters(self._article_title, self._article_seo_keywords)
+        
+        chapters_list_str = ', '.join(self._article_chapters)
+        chapters_description_list_str = ', '.join(self._article_chapters_header)
+        
+        self._logger.info("Article chapters: " + chapters_list_str)
+        self._logger.info("Article chapters headers: " + chapters_description_list_str)
+        self._cache.write_cache_article(self._cache.tag_chapters_list(), chapters_list_str)
+        self._cache.write_cache_article(self._cache.tag_chapters_list_header(), chapters_description_list_str)
     
     def get_title(self):
         return self._article_title
--- src/BlogCache.py
+++ src/BlogCache.py
@@ -4,59 +4,154 @@ import re
 STANDARD_STATUS_FILE_PATH = "src/.temp_gen/status.tmp"
 STANDARD_ARTICLE_FILE_PATH = "src/.temp_gen/article.tmp"
 
+CACHE_TAG_ARTICLE_TITLE = "TITLE"
+
 class BlogCache():
 
-    def __init__(self, status_file_path = STANDARD_STATUS_FILE_PATH, article_file_path = STANDARD_ARTICLE_FILE_PATH, pattern = r'###(\d+)###'):
+    def __init__(self, load_at_init, status_file_path = STANDARD_STATUS_FILE_PATH, article_file_path = STANDARD_ARTICLE_FILE_PATH, pattern = r'###(\d+)###'):
         self._status_file_path = status_file_path
         self._article_file_path = article_file_path
         self._pattern = pattern
-    
-    def load_cached_content(self):
-        cache_exists, cached_status, cached_article = self._read_cache_content()
 
-        chapter_content_list = []
-        if cached_status == "finished":
-            content_before_last_match = ""
+        self._cache_valid = False
+        self._cached_status = ""
+        self._cached_raw_content = ""
+        self._cache_read_until = 0
+
+        self._cached_title = ""
+        self._cached_seo_keywords = ""
+        self._cached_chapters_list = ""
+        self._cached_chapters_list_headers = ""
+        self._cached_chapters_content = ""
+        self._cached_reviewed_content = ""
+
+        self._cache_status_tags = {'IN_PROGRESS': 'PENDING',
+                                   'FINISHED': 'DONE'
+                                   }
+
+        self._cache_content_tags = {'CACHE_TAG_ARTICLE_TITLE': 'TITLE', 
+                                    'CACHE_TAG_ARTICLE_SEO_KEYWORDS': 'SEO_KEYWORDS', 
+                                    'CACHE_TAG_ARTICLE_CHAPTERS_LIST': 'CHAPTERS_LIST',
+                                    'CACHE_TAG_ARTICLE_CHAPTERS_LIST_HEADERS': 'CHAPTERS_LIST_HEADERS',
+                                    'CACHE_TAG_ARTICLE_CHAPTERS_CONTENT': 'CHAPTERS_CONTENT',
+                                    'CACHE_TAG_ARTICLE_REVIEWED_CONTENT': 'REVIEWED_CONTENT'
+                        }
+
+        if load_at_init:
+            self._read_cache()
+
+        if not self.is_valid():
+            #start a new cache
+            self._write_cache_status(self._cache_status_tags["IN_PROGRESS"])
         else:
-            if not cache_exists:
-                content_before_last_match = ""
-            else:
-                # Find all occurrences of the pattern in the text
-                matches = re.finditer(self._pattern, cached_article)
+            #recover content from cache
+            self._recreate_blog_article()
 
-                # Initialize the variable to store the content
-                match_end = 0
-                for match in matches:
-                    chapter_content_list.append(cached_article[match_end:match.start()])
-                    match_end = match.end()
+    def is_valid(self):
+        return self._cache_valid and self._cached_status != self._cache_status_tags["IN_PROGRESS"]
+    
+    def get_cached_article_title(self):
+        if self.is_valid(): return self._cached_title
+        else: return ""
 
-        return cache_exists, cached_status, chapter_content_list
+    def get_cached_article_seo_keywords(self):
+        if self.is_valid(): return self._cached_seo_keywords
+        else: return ""
+    
+    def get_cached_article_chapters_list(self):
+        if self.is_valid(): return self._cached_chapters_list
+        else: return ""
+    
+    def get_cached_article_chapters_list_headers(self):
+        if self.is_valid(): return self._cached_chapters_list_headers
+        else: return ""
+    
+    def get_cached_article_chapters_content(self):
+        if self.is_valid(): return self._cached_chapters_content
+        else: return ""
+    
+    def get_cached_article_reviewed_content(self):
+        if self.is_valid(): return self._cached_reviewed_content
+        else: return ""
+    
+    def tag_title(self):
+        return self._cache_content_tags["CACHE_TAG_ARTICLE_TITLE"]
+    
+    def tag_seo_keywords(self):
+        return self._cache_content_tags["CACHE_TAG_ARTICLE_SEO_KEYWORDS"]
+    
+    def tag_chapters_list(self):
+        return self._cache_content_tags["CACHE_TAG_ARTICLE_CHAPTERS_LIST"]
+    
+    def tag_chapters_list_header(self):
+        return self._cache_content_tags["CACHE_TAG_ARTICLE_CHAPTERS_LIST_HEADERS"]
+    
+    def tag_chapters_content(self):
+        return self._cache_content_tags["CACHE_TAG_ARTICLE_CHAPTERS_CONTENT"]
+    
+    def tag_reviewed_content(self):
+        return self._cache_content_tags["CACHE_TAG_ARTICLE_REVIEWED_CONTENT"]
 
     def write_cache_article(self, article_step, article_content):
-        with open(self._article_file_path, "w") as file:
-            file.write("###"+ article_step + "###")
-            file.write("###"+ article_content + "###")
-    
-    def write_cache_status(self, status):
+        with open(self._article_file_path, "a+") as file:
+            file.write("[" + article_step + "]\n"+ article_content + "\n[/" + article_step +  "]\n\n")
+
+    def _recreate_blog_article(self):
+
+        is_cached, self._cached_title = self._recreate_from_cached_content("CACHE_TAG_ARTICLE_TITLE") 
+        if not is_cached: return
+        
+        is_cached, self._cached_seo_keywords = self._recreate_from_cached_content("CACHE_TAG_ARTICLE_SEO_KEYWORDS")
+        if not is_cached: return
+
+        is_cached, self._cached_chapters_list = self._recreate_from_cached_content("CACHE_TAG_ARTICLE_CHAPTERS_LIST")
+        if not is_cached: return
+
+        is_cached, self._cached_chapters_list_headers = self._recreate_from_cached_content("CACHE_TAG_ARTICLE_CHAPTERS_LIST_HEADERS")
+        if not is_cached: return
+
+        is_cached, self._cached_chapters_content = self._recreate_from_cached_content("CACHE_TAG_ARTICLE_CHAPTERS_CONTENT")
+        if not is_cached: return
+
+        is_cached, self._cached_reviewed_content = self._recreate_from_cached_content("CACHE_TAG_ARTICLE_REVIEWED_CONTENT")
+        if not is_cached: return
+
+    def _recreate_from_cached_content(self, section_tag):
+        unread_cache = self._cached_raw_content[self._cache_read_until:]
+        match_found = False
+        match_content = ""
+
+        match_tag_open = re.search("[" + self._cache_content_tags[section_tag] + "]", unread_cache)
+        match_tag_closed = re.search("[/" + self._cache_content_tags[section_tag] + "]", unread_cache)
+
+        if match_tag_open and match_tag_closed:
+            self._cache_read_until = match_tag_closed.end()
+            match_content = self._cached_raw_content[match_tag_open.start():match_tag_closed.start()]
+            match_found = True
+        
+        return match_found, match_content
+
+    # # Find all occurrences of the pattern in the text
+    # matches = re.finditer(self._pattern, cached_article)
+
+    # # Initialize the variable to store the content
+    # match_end = 0
+    # for match in matches:
+    #     chapter_content_list.append(cached_article[match_end:match.start()])
+    #     match_end = match.end()
+
+
+    def _write_cache_status(self, status):
         with open(self._status_file_path, "w") as file:
             file.write(status)
 
-    def _read_cache_content(self):
+    def _read_cache(self):
 
-        if os.path.exists(self._status_file_path):
-            cache_exists = True
+        if os.path.exists(self._status_file_path) and os.path.exists(self._article_file_path):
+            self._cache_valid = True
+            
             with open(self._status_file_path, "r") as file:
-                cached_status = file.read()
+                self._cached_status = file.read()
 
-            if os.path.exists(self._article_file_path):
-                with open(self._article_file_path, "r") as file:
-                    cached_article = file.read()
-            else:
-                cached_article = ""
-                cache_exists = False
-        else:
-            cached_article = ""
-            cached_status = ""
-            cache_exists = False
-
-        return cache_exists, cached_status, cached_article
+            with open(self._article_file_path, "r") as file:
+                self._cached_raw_content = file.read()
